411bcadce186bb1ee8a22da9804c37847857b2aa
Emiro Puello Del Valle <epuellod1@unicartagena.edu.co>
2026-05-30 11:42:18 -0500

    Agrega intercambio de datos en el controlador y actualiza la vista index.html

--- src/main/java/com/puelloemiro/tutoria/ControladorInicio.java
+++ src/main/java/com/puelloemiro/tutoria/ControladorInicio.java
@@ -1,15 +1,29 @@
 package com.puelloemiro.tutoria;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value; // <-- Para leer el properties
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model; // <-- Importante para el intercambio de datos
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-@Slf4j // Esta anotación de Lombok nos permite usar el objeto 'log' automáticamente
+@Slf4j
 public class ControladorInicio {
 
-    @GetMapping("/") // Indica que este método responderá a la raíz de la aplicación (http://localhost:8080)
-    public String inicio() {
-        log.info("Ejecutando el controlador Inicio"); // Esto imprimirá un mensaje en la consola de NetBeans
-        return "index";    }
+    // Inyectamos el valor configurado en application.properties dentro de la variable 'dato'
+    @Value("${index.mensaje}")
+    private String dato;
+
+    @GetMapping("/")
+    public String inicio(Model modelo) { // <-- Pasamos el objeto 'modelo' como parámetro
+
+        String mensaje = "Saludos desde Spring MVC con paso de información";
+
+        // Agregamos los atributos al modelo (Clave, Valor)
+        modelo.addAttribute("mensaje", mensaje);
+        modelo.addAttribute("dato", dato);
+
+        log.info("Ejecutando el controlador Inicio MVC con intercambio de datos");
+        return "index";
+    }
 }
